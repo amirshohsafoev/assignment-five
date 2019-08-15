4c29444f5b768dbb218219c1a00cc2fee8ec2f83
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-15 19:42:54 -0400

    Services great practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UsersService } from "./users.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'assignment-five';
+export class AppComponent implements OnInit {
+  active: Array<string> = [];
+  inactive: Array<string> = [];
+
+  constructor(private usersService: UsersService) {}
+  ngOnInit() {
+    this.active = this.usersService.activeUsers;
+    this.inactive = this.usersService.inactiveUsers;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ActiveUsersComponent } from "./active-users/active-users.component";
+import { InactiveUsersComponent } from "./inactive-users/inactive-users.component";
+import { UsersService } from "./users.service";
+import { CounterService } from "./counter.service";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, ActiveUsersComponent, InactiveUsersComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [UsersService, CounterService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
